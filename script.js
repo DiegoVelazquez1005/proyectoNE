dba6facd5295f36272d0ee52eb4143b1ec8b9853
Eduardo4d4 <eduardo.tapia4d4@gmail.com>
2023-06-10 16:54:43 -0600

    Fotos + ventana emergente
    NOTA1: sepa que pasa con el top button
    
    NOTA2: A favor de las tablas como horarios
    
    NOTA3: El image map, pensaba mas en hacer un plano de una escuela, dividirlo en tres partes y que cada una lleve a primaria, secundaria o preparatoria
    
    Como ven x2 ¿?¿?

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('last-modified').innerHTML = document.lastModified;
 });
 
+function mostrarPersona(imagen, texto) {
+  document.getElementById('ventanaImagen').src = imagen;
+  document.getElementById('ventanaTexto').textContent = texto;
+  $('#ventanaPersona').modal('show');
+}
